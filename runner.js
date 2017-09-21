aa22a728efff7468be9ad55ef8afb4f2505273f4
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-09-21 19:23:52 +0700

    Release 0

--- runner.js
+++ runner.js
@@ -12,3 +12,35 @@ function sleep(milliseconds) {
 }
 
 // Your code here...
+
+let Racer = new JSRacer('A',10)
+let Racer1= new JSRacer('B',10)
+let temp=1;
+let temp1=1;
+console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+console.log(Racer.print_line('A',1).join('').toString())
+console.log(Racer.print_line('B',1).join('').toString())
+	setInterval(function () {
+		console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		temp=Racer.advanced_player(temp)
+		temp1=Racer.advanced_player(temp1)
+		if(temp>21){
+			temp=21
+		}
+
+		if(temp1>21){
+			temp1=21
+		}
+		
+		console.log(Racer.print_line('A',temp).join('').toString())
+		Racer.finished(temp)
+		if(temp==21){
+			process.exit()
+		}
+		console.log(Racer1.print_line('B',temp1).join('').toString())
+		Racer1.finished(temp1)
+		if(temp1==21){
+			process.exit()
+		}
+  	}, 1000);
+
