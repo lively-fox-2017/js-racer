e81caa290b3b9970a3d13f3dcb92b7d8dae19f11
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-21 13:49:45 +0700

    js racer berjalan

--- dice.js
+++ dice.js
@@ -2,11 +2,14 @@
 
 class Dice {
   constructor() {
-
+    this.dice = 0;
   }
   roll() {
-
+    let dice = Math.floor((Math.random()*6)+1);
+    this.dice = dice;
+    return this.dice;
   }
 }
+
 
 module.exports = Dice;
