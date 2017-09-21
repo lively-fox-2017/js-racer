f734ec4258bf228178157ecadef69ab50835e696
lokiloko <ianw.tkj2@gmail.com>
2017-09-21 15:46:33 +0700

    Release 1, added bomb and powerups. Added message dice and notif

--- js_racer.js
+++ js_racer.js
@@ -16,22 +16,42 @@ class JSRacer {
     }
     return temp;
   }
+  generateBomb(){
+    return [19,4,13];
+  }
+  generatePowerUp(){
+    return [1,12];
+  }
   print_board() {
     this.reset_board();
     var board = "";
     for(var i =0;i<this.players.length;i++){
+      if(this.generateBomb().indexOf(this.playersPos[i])!==-1){
+        this.playersPos[i] -= 2;
+        console.log(this.players[i] + " got BOMB! 2 move backwards");
+      }
+      else if(this.generatePowerUp().indexOf(this.playersPos[i])!==-1){
+        this.playersPos[i] += 8;
+        console.log(this.players[i] + " got POWER UP ! 8 move forwards");
+      }
       board += this.print_line(this.players[i],this.playersPos[i]);
     }
     return board;
   }
   print_line(player, pos) {
     var line = "";
-    for(var i=0;i<this.length;i++){
+    for(var i=0;i<=this.length;i++){
       if(i === pos){
-        line += `${player}|`;
+        line += `|${player}`;
+      }
+      else if(this.generateBomb().indexOf(i) !== -1){
+        line += `|*`;
+      }
+      else if(this.generatePowerUp().indexOf(i) !== -1){
+        line += '|o'
       }
       else{
-        line += " |";
+        line += "| ";
       }
     }
     return line+"\n";
@@ -40,16 +60,13 @@ class JSRacer {
     var dice = new Dice();
     var rollResult = dice.roll();
     this.playersPos[player] += rollResult;
-    if(this.playersPos[player] >= this.length-1){
-      this.playersPos[player] = this.length-1;
-      this.raceFinished = true;
-    }
+    console.log(this.players[player]+" Roll a dice : "+rollResult);
   }
   finished() {
     return this.raceFinished;
   }
   winner() {
-    var winner = this.playersPos.indexOf(this.length-1);
+    var winner = this.playersPos.indexOf(this.length);
     return `The winner is ${this.players[winner]}`;
   }
   reset_board() {
